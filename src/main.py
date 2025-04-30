3f59bffff6626d2592cd94d8407085f40861cf83
AndrewBetson <AndrewDCBetson@gmail.com>
2025-04-30 15:27:18 -0400

    Remove unnecessary input file extension check

--- src/main.py
+++ src/main.py
@@ -19,9 +19,6 @@ parser.add_argument( '-o', '--output', help='File to save converted MUS to', def
 args = parser.parse_args()
 
 midi_path: str = args.input
-if not midi_path.endswith( '.mid' ) and not midi_path.endswith( '.midi' ):
-	raise Exception( f'MIDI input doesn\'t end with .mid or .midi, please add the extension to the input.' )
-
 if not os.path.exists( midi_path ):
 	raise FileNotFoundError( f'Failed to locate provided MIDI file "{midi_path}".' )
 
